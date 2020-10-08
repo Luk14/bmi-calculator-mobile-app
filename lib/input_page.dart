2e3d665c6103e5410e27066727dc7d629f6dcc79
Luk14 <lukaszplayz121@gmail.com>
2020-10-08 10:44:52 +0100

    Video 135: Creating Custom Widgets

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+import 'icons/icon_content.dart';
+import 'icons/reusable_icon.dart';
 
 const bottomContainerHeight = 70.0;
 const activeCardColor = Color(0xFF1D1F33);
@@ -14,61 +18,57 @@ class _InputPageState extends State<InputPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('BMI CALCULATOR'),
-        ),
-        body: Column(
-          children: <Widget>[
-            Expanded(
-              child: Row(
-                children: <Widget>[
-                  Expanded(
-                    child: ReusableWidget(color: activeCardColor),
+      appBar: AppBar(
+        title: Text('BMI CALCULATOR'),
+      ),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: ReusableWidget(
+                    color: activeCardColor,
+                    cardChild: IconContent(
+                      icon: FontAwesomeIcons.mars,
+                      text: 'MALE',
+                    ),
                   ),
-                  Expanded(
-                    child: ReusableWidget(color: activeCardColor),
+                ),
+                Expanded(
+                  child: ReusableWidget(
+                    color: activeCardColor,
+                    cardChild: IconContent(
+                      icon: FontAwesomeIcons.venus,
+                      text: 'FEMALE',
+                    ),
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
-            Expanded(
-              child: ReusableWidget(color: activeCardColor),
+          ),
+          Expanded(
+            child: ReusableWidget(color: activeCardColor),
+          ),
+          Expanded(
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: ReusableWidget(color: activeCardColor),
+                ),
+                Expanded(
+                  child: ReusableWidget(color: activeCardColor),
+                ),
+              ],
             ),
-            Expanded(
-              child: Row(
-                children: <Widget>[
-                  Expanded(
-                    child: ReusableWidget(color: activeCardColor),
-                  ),
-                  Expanded(
-                    child: ReusableWidget(color: activeCardColor),
-                  ),
-                ],
-              ),
-            ),
-            Container(
-              color: bottomContainerColor,
-              margin: EdgeInsets.only(top: 10),
-              width: double.infinity,
-              height: bottomContainerHeight,
-            )
-          ],
-        ));
-  }
-}
-
-class ReusableWidget extends StatelessWidget {
-  ReusableWidget({@required this.color});
-
-  final Color color;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.all(15),
-      decoration: BoxDecoration(
-        color: color,
-        borderRadius: BorderRadius.circular(10),
+          ),
+          Container(
+            color: bottomContainerColor,
+            margin: EdgeInsets.only(top: 10),
+            width: double.infinity,
+            height: bottomContainerHeight,
+          ),
+        ],
       ),
     );
   }
